498ccf9e4d064f50482045ef2338278db62b8393
Peeem3201 <pimmy3201@gmail.com>
2025-10-30 13:45:54 +0700

    Fix relative tileset path again

--- Map/Water_coasts_animation.tsx
+++ Map/Water_coasts_animation.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Water_coasts_animation" tilewidth="16" tileheight="16" tilecount="928" columns="29">
- <image source="../../../../../Users/Aspire/Downloads/Tiled_files/Water_coasts_animation.png" width="464" height="512"/>
+ <image source="Water_coasts_animation.png" width="464" height="512"/>
 </tileset>
